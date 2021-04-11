b03b776231dbdd51b7e761e2ae74e76423855dc5
Nipun Bhagat <npabhagat38@gmail.com>
2021-04-11 08:27:40 +0530

    Created Main Activity

--- app/src/main/java/com/example/cricketscoringapp/TeamOneActivity.kt
+++ app/src/main/java/com/example/cricketscoringapp/TeamOneActivity.kt
@@ -3,13 +3,9 @@ package com.example.cricketscoringapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class TeamOneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_team_one)
     }
-
-    
-
 }
